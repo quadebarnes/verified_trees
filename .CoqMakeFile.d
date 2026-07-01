cd02e3dca6b7c3001f4313cc8cdc129765ba67ca
Quade Barnes <quade.barnes@gmail.com>
2026-07-01 15:58:59 -0600

    Rename to Trees.v

--- .CoqMakeFile.d
+++
@@ -1,2 +0,0 @@
-BinaryTrees.vo BinaryTrees.glob BinaryTrees.v.beautified BinaryTrees.required_vo: BinaryTrees.v /home/quadebarnes/.opam/rocq-9.2/lib/rocq-runtime/rocqworker
-BinaryTrees.vos BinaryTrees.vok BinaryTrees.required_vos: BinaryTrees.v /home/quadebarnes/.opam/rocq-9.2/lib/rocq-runtime/rocqworker
