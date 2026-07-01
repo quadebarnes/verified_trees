63cb947490a953f473a056a7712b52d6f14ce1a6
Quade Barnes <quade.barnes@gmail.com>
2026-07-01 13:56:43 -0600

    Implement find

--- BinaryTrees.v
+++ BinaryTrees.v
@@ -5,8 +5,6 @@ Import ListNotations.
 Inductive tree : Type :=
   node (label : nat) (branches : list tree).
 
-
-
 Fixpoint rm (t : tree) (target : nat) : option tree :=
   match t with
   | node label branches => if label =? target 
@@ -23,4 +21,22 @@ Fixpoint rm (t : tree) (target : nat) : option tree :=
 
 Example test_rm1: rm (node 1 [(node 3 nil);(node 7 nil);(node 9 nil)]) 3 
   = Some (node 1 [(node 7 nil);(node 9 nil)]).
+Proof. reflexivity. Qed.
+
+Fixpoint find (t : tree) (target : nat) : option tree := 
+  match t with
+  | node label branches => if label =? target 
+                             then Some t 
+                             else (fix find_list (l : list tree) (val : nat) : option tree :=
+                                     match l with
+                                     | nil => None
+                                     | h::t => match find h val with 
+                                               | None => find_list t val
+                                               | tree => tree
+                                               end 
+                                      end) branches target
+  end.
+
+Example test_find1: find (node 1 [(node 3 [(node 14 nil)]);(node 7 nil);(node 9 nil)]) 3 
+  = Some (node 3 [(node 14 nil)]).
 Proof. reflexivity. Qed.
